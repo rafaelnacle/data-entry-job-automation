69a5a4ef6c6ec33375f5eb48c192d6435c803578
= <=>
2025-04-08 17:13:42 -0300

    fill the form with the real state information and save to a google sheet

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import time
+
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 # Scrape the links, address and price of the properties
 header = {
@@ -27,3 +31,28 @@ all_price_elements = soup.select(".PropertyCardWrapper span")
 all_prices = [price.get_text().replace("/mo", "").split("+")[0] for price in all_price_elements if "$" in price.text]
 print(f"\nCleaned {len(all_prices)} prices: ")
 print(all_prices)
+
+# Fill the Google form with Selenium
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=chrome_options)
+
+for n in range(len(all_links)):
+    driver.get("https://forms.gle/UXVK5UBrSCdgfyqr9")
+    time.sleep(2)
+
+    address = driver.find_element(By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    price = driver.find_element(By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    link = driver.find_element(By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    submit_btn = driver.find_element(By.XPATH, value='//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span')
+
+    address.send_keys(all_addresses[n])
+    price.send_keys(all_prices[n])
+    link.send_keys(all_links[n])
+    submit_btn.click()
+
+    time.sleep(1)
+    another_response = driver.find_element(By.XPATH, value='/html/body/div[1]/div[2]/div[1]/div/div[4]/a')
+    another_response.click()
+    time.sleep(2)
