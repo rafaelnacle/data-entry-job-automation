c4f4ce9f23fb8f34a2ffc27b914ca31844bb8817
= <=>
2025-04-08 17:02:52 -0300

    get all links, addresses and prices and clean the data

--- main.py
+++ main.py
@@ -13,4 +13,17 @@ response = requests.get("https://appbrewery.github.io/Zillow-Clone/", headers=he
 data = response.text
 soup = BeautifulSoup(data, "html.parser")
 
-print(soup.prettify())
+all_links_elements = soup.select(".StyledPropertyCardDataWrapper a")
+all_links = [link["href"] for link in all_links_elements]
+print(f"We found {len(all_links)} links of properties listings in total: \n")
+print(all_links)
+
+all_addresses_elements = soup.select(".StyledPropertyCardDataWrapper address")
+all_addresses = [address.get_text().replace(" | ", " ").strip() for address in all_addresses_elements]
+print(f"\nCleaned {len(all_addresses)} addresses: ")
+print(all_addresses)
+
+all_price_elements = soup.select(".PropertyCardWrapper span")
+all_prices = [price.get_text().replace("/mo", "").split("+")[0] for price in all_price_elements if "$" in price.text]
+print(f"\nCleaned {len(all_prices)} prices: ")
+print(all_prices)
